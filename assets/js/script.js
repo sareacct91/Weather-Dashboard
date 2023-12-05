b320c12507617b467506638cc9d03bdb820d4cd6
Meow Roar <sareacct91@gmail.com>
2023-12-05 14:51:38 -0800

    Bug fix: typo

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,7 +96,7 @@ function renderCurrentWeather(currentWeatherData) {
   <p>Wind: ${currentWeatherData.wind.speed} MP</p>
   <p>Humidity: ${currentWeatherData.main.humidity} %</p>`;
   // "append" html elements
-  currentWeatherDispEl.insertAdjacentHTML("beforeend",htmlStr); 
+  currentDispEl.insertAdjacentHTML("beforeend",htmlStr); 
 }
 
 // get weather for the current city and display
